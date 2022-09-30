5999456472d49970948a677d44dda0aa911f3c71
Lekshmi Sudhikumar <skumarlekshmi@gmail.com>
2022-09-30 13:47:45 +0100

    first iteration of bot

--- src/training/dynamite/MyBot.java
+++ src/training/dynamite/MyBot.java
@@ -15,7 +15,46 @@ public class MyBot implements Bot {
     public Move makeMove(Gamestate gamestate) {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-        return Move.R;
+        return gamestate.getRounds().size() < 100 ? Move.D : getWinnableMove(gamestate);
+    }
+
+    public Move getWinnableMove(Gamestate gamestate){
+        Move P1 = getAllMove();
+        if(gamestate.getRounds().size() >100){
+            Round lastRound = gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+            Move P2 = lastRound.getP2();
+
+            if (P2 == Move.P){
+                P1 = Move.S;
+            } else if (P2 == Move.S) {
+                P1 = Move.R;
+            } else if (P2 == Move.R) {
+                P1 = Move.P;
+            } else if(P2 == Move.D){
+                P1 = Move.W;
+            } else if (P2 == Move.W) {
+                P1 = getRandomMove();
+            }
+        }
+        return P1;
+    }
+
+//    public Move getDynamiteMoves(Gamestate gamestate){
+//        Move P1 = getAllMove();
+//        if (gamestate.)
+//    }
+
+    public Move getRandomMove() {
+        int randomNumberBetween0And3 = (int)Math.floor(Math.random() * 3.0);
+        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S};
+        return possibleMoves[randomNumberBetween0And3];
+    }
+
+    public Move getAllMove(){
+        int randomNumberBetween0And3 = (int)Math.floor(Math.random() * 3.0);
+        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S, Move.D};
+        return possibleMoves[randomNumberBetween0And3];
+
     }
 
 }
